e3459de1b2ac8750b2b779137b19266b35da4b63
Chirag Aggarwal <thechiragaggarwal@gmail.com>
2023-06-29 17:02:51 +0000

    Allow running as binary without cron
    Signed-off-by: Chirag Aggarwal <thechiragaggarwal@gmail.com>

--- entry.sh
+++ entry.sh
@@ -10,14 +10,23 @@ echo borgmatic $borgmaticver
 echo $borgver
 echo apprise $apprisever
 
-# Test crontab
-supercronic -test /etc/borgmatic.d/crontab.txt || exit 1
+if [ $# -eq 0 ]; then
+  # Test crontab
+  supercronic -test /etc/borgmatic.d/crontab.txt || exit 1
 
-# Start supercronic
-if [ -n "${SUPERCRONIC_EXTRA_FLAGS}" ]; then
+  # Start supercronic
+  if [ -n "${SUPERCRONIC_EXTRA_FLAGS}" ]; then
     echo "The variable SUPERCRONIC_EXTRA_FLAGS is not empty, using extra flags"
     supercronic $SUPERCRONIC_EXTRA_FLAGS /etc/borgmatic.d/crontab.txt
-else
+  else
     echo "The variable SUPERCRONIC_EXTRA_FLAGS is empty, starting normally"
     supercronic /etc/borgmatic.d/crontab.txt
+  fi
+else
+  if [ "$1" = "bash" ] || [ "$1" = "sh" ] || [ "$1" = "/bin/bash" ] || [ "$1" = "/bin/sh" ]; then
+    exec "$@"
+  else
+    borgmatic "$@"
+  fi
 fi
+
